52c4e6b2dfa87ba795ba6fc9c112c6099e0a4c09
neec <neec@localhost.localdomain>
2020-07-05 18:04:46 +0900

    1st

--- api/user/join.php
+++ api/user/join.php
@@ -8,6 +8,7 @@
 // ini_set('display_errors', 'On');
 // ini_set('error_reporting', E_ALL);
 
+require_once("../util.php");
 //-------------------------------------------------
 // 初期値
 //-------------------------------------------------
@@ -18,9 +19,9 @@ define('DEFAULT_MONEY', 3000);
 //-------------------------------------------------
 // 準備
 //-------------------------------------------------
-$dsn  = 'mysql:dbname=sgrpg;host=127.0.0.1';  // 接続先を定義
-$user = 'senpai';      // MySQLのユーザーID
-$pw   = 'indocurry';   // MySQLのパスワード
+//$dsn  = 'mysql:dbname=sgrpg;host=127.0.0.1';  // 接続先を定義
+//$user = 'senpai';      // MySQLのユーザーID
+//$pw   = 'indocurry';   // MySQLのパスワード
 
 // 実行したいSQL
 $sql1 = 'INSERT INTO User(lv, exp, money) VALUES(:lv, :exp, :money)';
---
+++ api/util.php
@@ -0,0 +1,4 @@
+<?php
+$dsn  = 'mysql:dbname=sgrpg;host=127.0.0.1';
+$user = 'senpai';      // MySQLのユーザーID
+$pw ='indocurry';
